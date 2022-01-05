3593febfd984e50c198ad414671bd661818b4608
Ryan Garrett <RyanGar46@gmail.com>
2022-01-05 03:02:56 -0500

    Add moon dimension

--- src/main/java/com/ryangar46/apollo/Apollo.java
+++ src/main/java/com/ryangar46/apollo/Apollo.java
@@ -1,19 +1,16 @@
 package com.ryangar46.apollo;
 
+import com.ryangar46.apollo.world.dimension.ModDimensions;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Apollo implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "apollo";
 	public static final Logger LOGGER = LogManager.getLogger("apollo");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModDimensions.register();
 	}
 }
