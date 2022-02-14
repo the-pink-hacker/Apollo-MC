525d52cb1e84ee5f9066629ccb78525dc8880b6b
Ryan Garrett <RyanGar46@gmail.com>
2022-02-14 01:02:44 -0500

    Apply gravity to minecart and boat

--- src/main/java/com/ryangar46/apollo/mixin/ExperienceOrbEntityMixin.java
+++ src/main/java/com/ryangar46/apollo/mixin/ExperienceOrbEntityMixin.java
@@ -11,7 +11,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ExperienceOrbEntity.class)
 public class ExperienceOrbEntityMixin {
     // Changes falling speed
-    @Inject(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ExperienceOrbEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V", shift = At.Shift.AFTER, ordinal = 0))
+    @Inject(
+            method = "tick()V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/ExperienceOrbEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V",
+                    shift = At.Shift.AFTER,
+                    ordinal = 0
+            )
+    )
     private void gravityFall(CallbackInfo ci) {
         Vec3d velocity = ((ExperienceOrbEntity)(Object)this).getVelocity();
         ((ExperienceOrbEntity)(Object)this).setVelocity(velocity.add(0.0d, 0.03d - (0.03d * GravityManager.getGravityMultiplier(((ExperienceOrbEntity)(Object)this).world)), 0.0d));
--- src/main/java/com/ryangar46/apollo/mixin/LivingEntityMixin.java
+++ src/main/java/com/ryangar46/apollo/mixin/LivingEntityMixin.java
@@ -9,13 +9,21 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
     // Changes falling speed
-    @ModifyVariable(method = "travel(Lnet/minecraft/util/math/Vec3d;)V", at = @At("STORE"), ordinal = 0)
+    @ModifyVariable(
+            method = "travel(Lnet/minecraft/util/math/Vec3d;)V",
+            at = @At("STORE"),
+            ordinal = 0
+    )
     private double gravityFall(double g) {
         return g * GravityManager.getGravityMultiplier(((LivingEntity)(Object)this).world);
     }
 
     // Changes damage calculation
-    @ModifyVariable(method = "handleFallDamage(FFLnet/minecraft/entity/damage/DamageSource;)Z", at = @At("HEAD"), ordinal = 0)
+    @ModifyVariable(
+            method = "handleFallDamage(FFLnet/minecraft/entity/damage/DamageSource;)Z",
+            at = @At("HEAD"),
+            ordinal = 0
+    )
     private float gravityDamage(float fallDistance) {
         return fallDistance * (float)GravityManager.getGravityMultiplier(((LivingEntity)(Object)this).world);
     }
