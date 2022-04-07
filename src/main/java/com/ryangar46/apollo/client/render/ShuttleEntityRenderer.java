c6de73a8f2e4a9b850d3be613b47918b6d9ce9f3
RyanGar46 <ryangar46@gmail.com>
2022-04-06 20:59:47 -0400

    Add shuttle model

---
+++ src/main/java/com/ryangar46/apollo/client/render/ShuttleEntityRenderer.java
@@ -0,0 +1,12 @@
+package com.ryangar46.apollo.client.render;
+
+import com.ryangar46.apollo.client.render.model.ShuttleEntityModel;
+import com.ryangar46.apollo.entity.vehicle.ShuttleEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class ShuttleEntityRenderer extends GeoEntityRenderer<ShuttleEntity> {
+    public ShuttleEntityRenderer(EntityRendererFactory.Context renderManager) {
+        super(renderManager, new ShuttleEntityModel());
+    }
+}
--- src/main/java/com/ryangar46/apollo/entity/vehicle/ShuttleEntity.java
+++ src/main/java/com/ryangar46/apollo/entity/vehicle/ShuttleEntity.java
@@ -9,9 +9,16 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class ShuttleEntity extends MobEntity implements ImplementedInventory {
+public class ShuttleEntity extends MobEntity implements ImplementedInventory, IAnimatable {
     private final DefaultedList<ItemStack> items = DefaultedList.ofSize(5, ItemStack.EMPTY);
+    private final AnimationFactory FACTORY = new AnimationFactory(this);
 
     public ShuttleEntity(EntityType<? extends ShuttleEntity> type, World world) {
         super(type, world);
@@ -37,5 +44,19 @@ public class ShuttleEntity extends MobEntity implements ImplementedInventory {
     @Override
     public boolean canPlayerUse(PlayerEntity player) {
         return true;
+    }
+
+    private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    public void registerControllers(AnimationData data) {
+        data.addAnimationController(new AnimationController<>(this, "controller", 0, this::predicate));
+    }
+
+    @Override
+    public AnimationFactory getFactory() {
+        return FACTORY;
     }
 }
