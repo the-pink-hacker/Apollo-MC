c17ac6c1c6b3d403cae2bc2a8b09dfe8388f6535
RyanGar46 <ryangar46@gmail.com>
2022-04-13 23:36:16 -0400

    Add earth to the moon's sky

---
+++ src/main/java/com/ryangar46/apollo/mixin/client/render/WorldRendererMixin.java
@@ -0,0 +1,36 @@
+package com.ryangar46.apollo.mixin.client.render;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.ryangar46.apollo.Apollo;
+import com.ryangar46.apollo.client.render.SkyManager;
+import com.ryangar46.apollo.tag.TagManager;
+import com.ryangar46.apollo.world.dimension.DimensionManager;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(WorldRenderer.class)
+public abstract class WorldRendererMixin {
+    @Shadow
+    private ClientWorld world;
+
+    @Redirect(
+            method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderTexture(ILnet/minecraft/util/Identifier;)V",
+                    ordinal = 1
+            )
+    )
+    private void setMoon(int i, Identifier identifier) {
+        if (world.method_40134().isIn(TagManager.EARTH_VISIBLE_WORLDS)) {
+            RenderSystem.setShaderTexture(i, SkyManager.EARTH);
+        } else {
+            RenderSystem.setShaderTexture(i, identifier);
+        }
+    }
+}
