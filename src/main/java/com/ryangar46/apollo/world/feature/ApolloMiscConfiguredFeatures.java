baaa68adb8df421f347957e2e4b1ecb4d8c6bb91
Ryan Garrett <ryangar46@gmail.com>
2022-06-09 12:59:05 -0400

    Increase oil lake likelihood and format

--- src/main/java/com/ryangar46/apollo/world/feature/ApolloMiscConfiguredFeatures.java
+++ src/main/java/com/ryangar46/apollo/world/feature/ApolloMiscConfiguredFeatures.java
@@ -10,6 +10,36 @@ import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 
 public class ApolloMiscConfiguredFeatures {
-    public static final RegistryEntry<ConfiguredFeature<LakeFeature.Config, ?>> LAKE_OIL = ConfiguredFeatures.register("lake_oil", Feature.LAKE, new LakeFeature.Config(BlockStateProvider.of(BlockManager.OIL), BlockStateProvider.of(Blocks.STONE.getDefaultState())));
-    public static final RegistryEntry<ConfiguredFeature<SpringFeatureConfig, ?>> SPRING_OIL = ConfiguredFeatures.register("spring_oil", Feature.SPRING_FEATURE, new SpringFeatureConfig(FluidManager.STILL_OIL.getDefaultState(), true, 4, 1, RegistryEntryList.of(Block::getRegistryEntry, Blocks.STONE, Blocks.GRANITE, Blocks.DIORITE, Blocks.ANDESITE, Blocks.DEEPSLATE, Blocks.TUFF, Blocks.CALCITE, Blocks.DIRT, Blocks.SNOW_BLOCK, Blocks.POWDER_SNOW, Blocks.PACKED_ICE)));
+    public static final RegistryEntry<ConfiguredFeature<LakeFeature.Config, ?>> LAKE_OIL = ConfiguredFeatures.register(
+            "lake_oil",
+            Feature.LAKE,
+            new LakeFeature.Config(
+                    BlockStateProvider.of(BlockManager.OIL),
+                    BlockStateProvider.of(Blocks.STONE.getDefaultState())
+            )
+    );
+    public static final RegistryEntry<ConfiguredFeature<SpringFeatureConfig, ?>> SPRING_OIL = ConfiguredFeatures.register(
+            "spring_oil",
+            Feature.SPRING_FEATURE,
+            new SpringFeatureConfig(
+                    FluidManager.STILL_OIL.getDefaultState(),
+                    true,
+                    4,
+                    1,
+                    RegistryEntryList.of(
+                            Block::getRegistryEntry,
+                            Blocks.STONE,
+                            Blocks.GRANITE,
+                            Blocks.DIORITE,
+                            Blocks.ANDESITE,
+                            Blocks.DEEPSLATE,
+                            Blocks.TUFF,
+                            Blocks.CALCITE,
+                            Blocks.DIRT,
+                            Blocks.SNOW_BLOCK,
+                            Blocks.POWDER_SNOW,
+                            Blocks.PACKED_ICE
+                    )
+            )
+    );
 }
--- src/main/java/com/ryangar46/apollo/world/feature/ApolloMiscPlacedFeatures.java
+++ src/main/java/com/ryangar46/apollo/world/feature/ApolloMiscPlacedFeatures.java
@@ -12,7 +12,48 @@ import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 import net.minecraft.world.gen.placementmodifier.*;
 
 public class ApolloMiscPlacedFeatures {
-    public static final RegistryEntry<PlacedFeature> LAKE_OIL_UNDERGROUND = PlacedFeatures.register("lake_oil_underground", ApolloMiscConfiguredFeatures.LAKE_OIL, RarityFilterPlacementModifier.of(9), SquarePlacementModifier.of(), HeightRangePlacementModifier.of(UniformHeightProvider.create(YOffset.fixed(0), YOffset.getTop())), EnvironmentScanPlacementModifier.of(Direction.DOWN, BlockPredicate.bothOf(BlockPredicate.not(BlockPredicate.IS_AIR), BlockPredicate.insideWorldBounds(new BlockPos(0, -5, 0))), 32), SurfaceThresholdFilterPlacementModifier.of(Heightmap.Type.OCEAN_FLOOR_WG, Integer.MIN_VALUE, -5), BiomePlacementModifier.of());
-    public static final RegistryEntry<PlacedFeature> LAKE_OIL_SURFACE = PlacedFeatures.register("lake_oil_surface", ApolloMiscConfiguredFeatures.LAKE_OIL, RarityFilterPlacementModifier.of(200), SquarePlacementModifier.of(), PlacedFeatures.WORLD_SURFACE_WG_HEIGHTMAP, BiomePlacementModifier.of());
-    public static final RegistryEntry<PlacedFeature> SPRING_OIL = PlacedFeatures.register("spring_oil", ApolloMiscConfiguredFeatures.SPRING_OIL, CountPlacementModifier.of(25), SquarePlacementModifier.of(), HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(192)), BiomePlacementModifier.of());
+    public static final RegistryEntry<PlacedFeature> LAKE_OIL_UNDERGROUND = PlacedFeatures.register(
+            "lake_oil_underground",
+            ApolloMiscConfiguredFeatures.LAKE_OIL,
+            RarityFilterPlacementModifier.of(3),
+            SquarePlacementModifier.of(),
+            HeightRangePlacementModifier.of(
+                    UniformHeightProvider.create(
+                            YOffset.fixed(0),
+                            YOffset.getTop()
+                    )
+            ),
+            EnvironmentScanPlacementModifier.of(
+                    Direction.DOWN,
+                    BlockPredicate.bothOf(
+                            BlockPredicate.not(BlockPredicate.IS_AIR),
+                            BlockPredicate.insideWorldBounds(new BlockPos(0, -5, 0))
+                    ),
+                    32
+            ),
+            SurfaceThresholdFilterPlacementModifier.of(
+                    Heightmap.Type.OCEAN_FLOOR_WG,
+                    Integer.MIN_VALUE, -5
+            ),
+            BiomePlacementModifier.of()
+    );
+    public static final RegistryEntry<PlacedFeature> LAKE_OIL_SURFACE = PlacedFeatures.register(
+            "lake_oil_surface",
+            ApolloMiscConfiguredFeatures.LAKE_OIL,
+            RarityFilterPlacementModifier.of(50),
+            SquarePlacementModifier.of(),
+            PlacedFeatures.WORLD_SURFACE_WG_HEIGHTMAP,
+            BiomePlacementModifier.of()
+    );
+    public static final RegistryEntry<PlacedFeature> SPRING_OIL = PlacedFeatures.register(
+            "spring_oil",
+            ApolloMiscConfiguredFeatures.SPRING_OIL,
+            CountPlacementModifier.of(25),
+            SquarePlacementModifier.of(),
+            HeightRangePlacementModifier.uniform(
+                    YOffset.getBottom(),
+                    YOffset.fixed(192)
+            ),
+            BiomePlacementModifier.of()
+    );
 }
