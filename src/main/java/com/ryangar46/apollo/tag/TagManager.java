b3068c71b4695487b5e20017fc3ed1698c418a0a
Ryan Garrett <ryangar46@gmail.com>
2022-06-09 20:51:49 -0400

    Fix blockstate desync issues

--- src/main/java/com/ryangar46/apollo/tag/TagManager.java
+++ src/main/java/com/ryangar46/apollo/tag/TagManager.java
@@ -17,23 +17,24 @@ public class TagManager {
     public static final TagKey<DimensionType> ATMOSPHERE_NOT_VISIBLE_WORLDS = registerDimensionType("atmosphere_not_visible_worlds");
     public static final TagKey<DimensionType> EARTH_VISIBLE_WORLDS = registerDimensionType("earth_visible_worlds");
     public static final TagKey<Block> FLUID_PIPE_CONNECTABLE_BLOCKS = registerBlock("fluid_pipe_connectable_blocks");
+    public static final TagKey<Block> LAUNCHPADS = registerBlock("launchpads");
     public static final TagKey<Item> NEGATIVE_GRAVITY_EQUIPABLES = registerItem("negative_gravity_equipables");
     public static final TagKey<Item> POSITIVE_GRAVITY_EQUIPABLES = registerItem("positive_gravity_equipables");
     public static final TagKey<EntityType<?>> VACUUM_IMMUNE_CREATURES = registerEntityType("vacuum_immune_creatures");
 
-    public static TagKey<Item> registerItem(String id) {
+    private static TagKey<Item> registerItem(String id) {
         return TagKey.of(Registry.ITEM_KEY, new Identifier(Apollo.MOD_ID, id));
     }
 
-    public static TagKey<Block> registerBlock(String id) {
+    private static TagKey<Block> registerBlock(String id) {
         return TagKey.of(Registry.BLOCK_KEY, new Identifier(Apollo.MOD_ID, id));
     }
 
-    public static TagKey<EntityType<?>> registerEntityType(String id) {
+    private static TagKey<EntityType<?>> registerEntityType(String id) {
         return TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier(Apollo.MOD_ID, id));
     }
 
-    public static TagKey<DimensionType> registerDimensionType(String id) {
+    private static TagKey<DimensionType> registerDimensionType(String id) {
         return TagKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier(Apollo.MOD_ID, id));
     }
 }
