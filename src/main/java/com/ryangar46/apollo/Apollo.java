99c0b2f79737e7e11fa11e30446ba829d1b79fe6
Ryan Garrett <ryangar46@gmail.com>
2022-07-13 20:37:06 -0400

    Fix issue with client code running on the server
    This caused a crash on the dedicated server since the classes couldn't be found.

--- src/main/java/com/ryangar46/apollo/Apollo.java
+++ src/main/java/com/ryangar46/apollo/Apollo.java
@@ -2,7 +2,6 @@ package com.ryangar46.apollo;
 
 import com.ryangar46.apollo.block.BlockManager;
 import com.ryangar46.apollo.entity.EntityManager;
-import com.ryangar46.apollo.fluid.FluidManager;
 import com.ryangar46.apollo.item.ItemManager;
 import com.ryangar46.apollo.recipe.RecipeManager;
 import com.ryangar46.apollo.stat.StatManager;
@@ -29,7 +28,6 @@ public class Apollo implements ModInitializer, TerraBlenderApi {
 		GameRuleManager.register();
 		ItemManager.register();
 		BlockManager.register();
-		FluidManager.register();
 		EntityManager.register();
 		BiomeManager.register();
 		RecipeManager.register();
