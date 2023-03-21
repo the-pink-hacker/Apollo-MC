fcfa1abead2b6b6a5d7f059a78f4c8a2d9b10af0
Ryan Garrett <RyanGar46@gmail.com>
2023-03-20 22:25:36 -0400

    Add airlock

---
+++ src/main/java/com/ryangar46/apollo/block/AirlockBlock.java
@@ -0,0 +1,9 @@
+package com.ryangar46.apollo.block;
+
+import net.minecraft.block.Block;
+
+public class AirlockBlock extends Block {
+    public AirlockBlock(Settings settings) {
+        super(settings);
+    }
+}
