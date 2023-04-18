2b085a1212c99230e57c225867350aca3af0d656
Ryan Garrett <RyanGar46@gmail.com>
2023-04-18 17:50:51 -0400

    Add input output fluid carrier class

--- src/main/java/com/thepinkhacker/apollo/block/entity/fluid/OilRefineryBlockEntity.java
+++ src/main/java/com/thepinkhacker/apollo/block/entity/fluid/OilRefineryBlockEntity.java
@@ -1,43 +1,91 @@
 package com.thepinkhacker.apollo.block.entity.fluid;
 
 import com.thepinkhacker.apollo.block.entity.ApolloBlockEntityTypes;
+import com.thepinkhacker.apollo.fluid.ApolloFluidConstants;
 import com.thepinkhacker.apollo.fluid.FluidCarrier;
 import com.thepinkhacker.apollo.fluid.FluidCarrierStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.Nullable;
 
-public class OilRefineryBlockEntity extends BlockEntity implements FluidCarrier<OilRefineryBlockEntity> {
+public class OilRefineryBlockEntity extends InputOutputFluidCarrier<OilRefineryBlockEntity> {
+    private static final String STORAGE_VARIANT_NBT_TAG = "storage_fluid_variant";
+    private static final String STORAGE_AMOUNT_NBT_TAG = "storage_fluid_amount";
+    private static final String INPUT_STORAGE_VARIANT_NBT_TAG = STORAGE_VARIANT_NBT_TAG + "_input";
+    private static final String INPUT_STORAGE_AMOUNT_NBT_TAG = STORAGE_AMOUNT_NBT_TAG + "_input";
+    private static final String OUTPUT_STORAGE_VARIANT_NBT_TAG = STORAGE_VARIANT_NBT_TAG + "_output";
+    private static final String OUTPUT_STORAGE_AMOUNT_NBT_TAG = STORAGE_AMOUNT_NBT_TAG + "_output";
+    public final FluidCarrierStorage<OilRefineryBlockEntity> FLUID_CARRIER_STORAGE_INPUT = new FluidCarrierStorage<>(this);
+    public final FluidCarrierStorage<OilRefineryBlockEntity> FLUID_CARRIER_STORAGE_OUTPUT = new FluidCarrierStorage<>(this);
+    public final FluidStorage INPUT_STORAGE = new FluidStorage(this);
+    public final FluidStorage OUTPUT_STORAGE = new FluidStorage(this);
+
     public OilRefineryBlockEntity(BlockPos pos, BlockState state) {
         super(ApolloBlockEntityTypes.OIL_REFINERY, pos, state);
     }
 
-    @Nullable
     @Override
-    public FluidCarrierStorage<OilRefineryBlockEntity> getFluidCarrierStorage(Direction direction) {
-        return FluidCarrier.super.getFluidCarrierStorage(direction);
+    public FluidCarrierStorage<OilRefineryBlockEntity> getInputCarrier() {
+        return FLUID_CARRIER_STORAGE_INPUT;
     }
 
     @Override
-    public FluidCarrierStorage<OilRefineryBlockEntity> getMainCarrierStorage() {
-        return null;
+    public FluidCarrierStorage<OilRefineryBlockEntity> getOutputCarrier() {
+        return FLUID_CARRIER_STORAGE_OUTPUT;
     }
 
     @Override
-    public void writeFluidCarrier(NbtCompound nbt) {
-        FluidCarrier.super.writeFluidCarrier(nbt);
+    public SingleVariantStorage<FluidVariant> getInputStorage() {
+        return INPUT_STORAGE;
     }
 
     @Override
-    public void readFluidCarrier(NbtCompound nbt) {
-        FluidCarrier.super.readFluidCarrier(nbt);
+    public SingleVariantStorage<FluidVariant> getOutputStorage() {
+        return OUTPUT_STORAGE;
     }
 
     @Override
-    public boolean checkFluidCarrierDirection(Direction direction) {
-        return FluidCarrier.super.checkFluidCarrierDirection(direction);
+    protected void writeStorage(NbtCompound nbt) {
+        nbt.put(INPUT_STORAGE_VARIANT_NBT_TAG, INPUT_STORAGE.variant.toNbt());
+        nbt.putLong(INPUT_STORAGE_AMOUNT_NBT_TAG, INPUT_STORAGE.amount);
+        nbt.put(OUTPUT_STORAGE_VARIANT_NBT_TAG, OUTPUT_STORAGE.variant.toNbt());
+        nbt.putLong(OUTPUT_STORAGE_AMOUNT_NBT_TAG, OUTPUT_STORAGE.amount);
     }
+
+    @Override
+    protected void readStorage(NbtCompound nbt) {
+        INPUT_STORAGE.variant = FluidVariant.fromNbt(nbt.getCompound(INPUT_STORAGE_VARIANT_NBT_TAG));
+        INPUT_STORAGE.amount = nbt.getLong(INPUT_STORAGE_AMOUNT_NBT_TAG);
+        OUTPUT_STORAGE.variant = FluidVariant.fromNbt(nbt.getCompound(OUTPUT_STORAGE_VARIANT_NBT_TAG));
+        OUTPUT_STORAGE.amount = nbt.getLong(OUTPUT_STORAGE_AMOUNT_NBT_TAG);
+    }
+
+    private static class FluidStorage extends SingleVariantStorage<FluidVariant> {
+        private final OilRefineryBlockEntity parent;
+
+        public FluidStorage(OilRefineryBlockEntity parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        protected FluidVariant getBlankVariant() {
+            return FluidVariant.blank();
+        }
+
+        @Override
+        protected long getCapacity(FluidVariant variant) {
+            return ApolloFluidConstants.BLOCK_CAPACITY;
+        }
+
+        @Override
+        protected void onFinalCommit() {
+            this.parent.markDirty();
+        }
+    };
 }
