840ea587a58056529de3fb194e0a9fa465e1d0cd
Ryan Garrett <RyanGar46@gmail.com>
2023-05-22 16:09:53 -0400

    Add Apollo dimension type

--- src/main/java/com/thepinkhacker/apollo/resource/SpaceBody.java
+++ src/main/java/com/thepinkhacker/apollo/resource/SpaceBody.java
@@ -1,28 +1,6 @@
 package com.thepinkhacker.apollo.resource;
 
-public class SpaceBody {
-    private final double gravity;
-    private final boolean isAtmosphereVisible;
-    private final boolean hasOxygen;
-
-    public SpaceBody(double gravity, boolean isAtmosphereVisible, boolean hasOxygen) {
-        this.gravity = gravity;
-        this.isAtmosphereVisible = isAtmosphereVisible;
-        this.hasOxygen = hasOxygen;
-    }
-
-    public double getGravity() {
-        return gravity;
-    }
-
-    public boolean getIsAtmosphereVisible() {
-        return isAtmosphereVisible;
-    }
-
-    public boolean getHasOxygen() {
-        return hasOxygen;
-    }
-
+public record SpaceBody(double gravity, boolean isAtmosphereVisible, boolean hasOxygen) {
     @Override
     public String toString() {
         return "gravity: " + gravity +
