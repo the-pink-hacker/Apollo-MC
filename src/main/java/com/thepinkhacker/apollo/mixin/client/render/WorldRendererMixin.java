63e9ea2d5b7560960157e7e10af9c0ee92804255
Ryan Garrett <RyanGar46@gmail.com>
2023-06-01 14:03:58 -0400

    Inject into calculate world ambient darkness

--- src/main/java/com/thepinkhacker/apollo/mixin/client/render/WorldRendererMixin.java
+++ src/main/java/com/thepinkhacker/apollo/mixin/client/render/WorldRendererMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import org.joml.Matrix4f;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,7 +15,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Debug(export = true)
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
     @Shadow @Final private MinecraftClient client;
--- src/main/java/com/thepinkhacker/apollo/world/dimension/DayCycleManager.java
+++ src/main/java/com/thepinkhacker/apollo/world/dimension/DayCycleManager.java
@@ -1,10 +1,16 @@
 package com.thepinkhacker.apollo.world.dimension;
 
 import com.thepinkhacker.apollo.resource.SpaceBodyManager;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 // TODO: Add improved time command
 public abstract class DayCycleManager {
+    private static final long TICKS_PER_SECOND = 20;
+    private static final long MINUTES_PER_SECOND = 20;
+    private static final long SECONDS_PER_DAY = 60 * MINUTES_PER_SECOND;
+    private static final long TICKS_PER_DAY = TICKS_PER_SECOND * SECONDS_PER_DAY;
+    private static final long TICKS_PER_QUARTER_DAY = TICKS_PER_DAY / 4;
     public static WorldTime getLightProviderTime(long ticks, SpaceBody spaceBody) {
         return WorldTime.ofOrbitTicks(ticks, spaceBody.getLightProvider().getOrbit());
     }
@@ -21,15 +27,20 @@ public abstract class DayCycleManager {
         return getLightProviderTime(world.getLunarTime(), world);
     }
 
+    public static double getSkyAngleDegrees(WorldTime time) {
+        double d = MathHelper.fractionalPart((double)time.asTicks() / TICKS_PER_DAY - 0.25d);
+        double e = 0.5d - Math.cos(d * Math.PI) / 2.0d;
+        return (d * 2.0d + e) / 3.0d * 360.0d;
+    }
+
+    public static double getSkyAngleDegreesLightProvider(World world) {
+        return getSkyAngleDegrees(getLightProviderTime(world));
+    }
+
     public static class WorldTime {
-        private static final long TICKS_PER_SECOND = 20;
-        private static final long MINUTES_PER_SECOND = 20;
-        private static final long SECONDS_PER_DAY = 60 * MINUTES_PER_SECOND;
-        private static final long TICKS_PER_DAY = TICKS_PER_SECOND * SECONDS_PER_DAY;
-        private static final long TICKS_PER_QUARTER_DAY = TICKS_PER_DAY / 4;
-        public static final WorldTime DAY = WorldTime.ofOverworldTicks(0);
+        public static final WorldTime DAY = WorldTime.ofOverworldTicks(1_000);
         public static final WorldTime NOON = WorldTime.ofOverworldTicks(TICKS_PER_QUARTER_DAY);
-        public static final WorldTime NIGHT = WorldTime.ofOverworldTicks(TICKS_PER_QUARTER_DAY * 2);
+        public static final WorldTime NIGHT = WorldTime.ofOverworldTicks(13_000);
         public static final WorldTime MIDNIGHT = WorldTime.ofOverworldTicks(TICKS_PER_QUARTER_DAY * 3);
         private final long overworldTicks;
 
@@ -41,16 +52,16 @@ public abstract class DayCycleManager {
             return new WorldTime(ticks);
         }
 
-        public long inTicks() {
+        public long asTicks() {
             return overworldTicks;
         }
 
         public static WorldTime ofOrbitTicks(long ticks, SpaceBody.Orbit orbit) {
-            return ofOverworldTicks((long)(ticks / orbit.getTimeMultiplier()));
+            return ofOverworldTicks((long)(ticks * orbit.getTimeMultiplier()));
         }
 
-        public long inOrbitTicks(SpaceBody.Orbit orbit) {
-            return (long)(inTicks() * orbit.getTimeMultiplier());
+        public long asOrbitTicks(SpaceBody.Orbit orbit) {
+            return (long)(asTicks() / orbit.getTimeMultiplier());
         }
     }
 }
