1473991abab013744084ec97e56bc3cf03d81c50
Ryan Garrett <ryan@thepinkhacker.com>
2023-06-03 22:16:32 -0400

    Disable mixin export

--- src/main/java/com/thepinkhacker/apollo/mixin/resource/featuretoggle/FeatureFlagsMixin.java
+++ src/main/java/com/thepinkhacker/apollo/mixin/resource/featuretoggle/FeatureFlagsMixin.java
@@ -3,12 +3,10 @@ package com.thepinkhacker.apollo.mixin.resource.featuretoggle;
 import com.thepinkhacker.apollo.resource.featuretoggle.ApolloFeatureFlags;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.resource.featuretoggle.FeatureManager;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Debug(export = true)
 @Mixin(FeatureFlags.class)
 public abstract class FeatureFlagsMixin {
     @ModifyVariable(
