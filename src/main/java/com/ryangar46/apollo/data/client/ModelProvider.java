21c4a3d8e06171a821b6cf7364b888abd0ac9d27
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:50 -0400

    Move cube all models to data generators

--- src/main/java/com/ryangar46/apollo/data/client/ModelProvider.java
+++ src/main/java/com/ryangar46/apollo/data/client/ModelProvider.java
@@ -1,17 +1,17 @@
 package com.ryangar46.apollo.data.client;
 
 import com.ryangar46.apollo.Apollo;
+import com.ryangar46.apollo.block.ApolloBlocks;
 import com.ryangar46.apollo.item.ApolloItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.block.Block;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Model;
 import net.minecraft.data.client.Models;
+import net.minecraft.data.client.TexturedModel;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
-
-import java.util.Optional;
 
 public class ModelProvider extends FabricModelProvider {
     public ModelProvider(FabricDataOutput output) {
@@ -19,63 +19,64 @@ public class ModelProvider extends FabricModelProvider {
     }
 
     @Override
-    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {}
+    public void generateBlockStateModels(BlockStateModelGenerator generator) {
+        /* === Cube All === */
+        generator.registerSimpleCubeAll(ApolloBlocks.AIRLOCK_CONTROLLER);
+        generator.registerSimpleCubeAll(ApolloBlocks.AIRLOCK_FRAME);
+        generator.registerRotatable(ApolloBlocks.LUNAR_COBBLESTONE);
+        generator.registerRotatable(ApolloBlocks.LUNAR_DUST);
+        generator.registerSimpleCubeAll(ApolloBlocks.LUNAR_IRON_ORE);
+        generator.registerRotatable(ApolloBlocks.LUNAR_SOIL);
+        generator.registerRotatable(ApolloBlocks.LUNAR_STONE);
+        generator.registerSimpleCubeAll(ApolloBlocks.REINFORCED_IRON_BLOCK);
+
+        /* === Items === */
+        registerSimpleBlockItem(generator, ApolloBlocks.AIRLOCK_CONTROLLER);
+        registerSimpleBlockItem(generator, ApolloBlocks.AIRLOCK_FRAME);
+        registerSimpleBlockItem(generator, ApolloBlocks.FLUID_PIPE, "fluid_pipe_inv");
+        registerSimpleBlockItem(generator, ApolloBlocks.LAUNCHPAD, "launchpad_inv");
+        registerSimpleBlockItem(generator, ApolloBlocks.LUNAR_COBBLESTONE);
+        registerSimpleBlockItem(generator, ApolloBlocks.LUNAR_DUST);
+        registerSimpleBlockItem(generator, ApolloBlocks.LUNAR_IRON_ORE);
+        registerSimpleBlockItem(generator, ApolloBlocks.LUNAR_SOIL);
+        registerSimpleBlockItem(generator, ApolloBlocks.LUNAR_STONE);
+        registerSimpleBlockItem(generator, ApolloBlocks.METEORITE, "meteorite_cool");
+        registerSimpleBlockItem(generator, ApolloBlocks.OIL_REFINERY);
+        registerSimpleBlockItem(generator, ApolloBlocks.OXYGEN_PLANT, "oxygen_plant_stage3");
+        registerSimpleBlockItem(generator, ApolloBlocks.REINFORCED_IRON_BLOCK);
+        registerSimpleBlockItem(generator, ApolloBlocks.SHUTTLE_WORKBENCH);
+        registerSimpleBlockItem(generator, ApolloBlocks.STORAGE_TANK);
+    }
 
     @Override
-    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-        /* === Blocks === */
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.AIRLOCK_CONTROLLER);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.AIRLOCK_FRAME);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.FLUID_PIPE, "fluid_pipe_inv");
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.LAUNCHPAD, "launchpad_inv");
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.LUNAR_COBBLESTONE);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.LUNAR_DUST);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.LUNAR_IRON_ORE);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.LUNAR_SOIL);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.LUNAR_STONE);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.METEORITE, "meteorite_cool");
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.OIL_REFINERY);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.OXYGEN_PLANT, "oxygen_plant_stage3");
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.REINFORCED_IRON_BLOCK);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.SHUTTLE_WORKBENCH);
-        registerBasicBlockItem(itemModelGenerator, ApolloItems.STORAGE_TANK);
-
-        /* === Generated === */
-        itemModelGenerator.register(ApolloItems.FUEL_BUCKET, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.METEORITE_SCRAP, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.OIL_BUCKET, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.OXYGEN_PLANT_SEEDS, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.NEGATIVE_GRAVITY_BOOTS, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.POSITIVE_GRAVITY_BOOTS, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.REINFORCED_IRON_INGOT, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.SPACE_SUIT_BOOTS, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.SPACE_SUIT_CHESTPLATE, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.SPACE_SUIT_HELMET, Models.GENERATED);
-        itemModelGenerator.register(ApolloItems.SPACE_SUIT_LEGGINGS, Models.GENERATED);
+    public void generateItemModels(ItemModelGenerator generator) {
+        registerGeneratedItem(generator, ApolloItems.FUEL_BUCKET);
+        registerGeneratedItem(generator, ApolloItems.METEORITE_SCRAP);
+        registerGeneratedItem(generator, ApolloItems.OIL_BUCKET);
+        registerGeneratedItem(generator, ApolloItems.OXYGEN_PLANT_SEEDS);
+        registerGeneratedItem(generator, ApolloItems.NEGATIVE_GRAVITY_BOOTS);
+        registerGeneratedItem(generator, ApolloItems.POSITIVE_GRAVITY_BOOTS);
+        registerGeneratedItem(generator, ApolloItems.REINFORCED_IRON_INGOT);
+        registerGeneratedItem(generator, ApolloItems.SPACE_SUIT_BOOTS);
+        registerGeneratedItem(generator, ApolloItems.SPACE_SUIT_CHESTPLATE);
+        registerGeneratedItem(generator, ApolloItems.SPACE_SUIT_HELMET);
+        registerGeneratedItem(generator, ApolloItems.SPACE_SUIT_LEGGINGS);
     }
 
-    private static Model basicBlockItem(Identifier parent) {
-        return new Model(Optional.of(parent), Optional.empty());
+    private static void registerSimpleBlockItem(BlockStateModelGenerator generator, Block block) {
+        Identifier parentIdentifier = block.getRegistryEntry().registryKey().getValue();
+        generator.registerParentedItemModel(block, new Identifier(parentIdentifier.getNamespace(), "block/" + parentIdentifier.getPath()));
     }
 
-    private static Model basicBlockItem(String parent) {
-        return basicBlockItem(new Identifier(Apollo.MOD_ID, "block/" + parent));
+    private static void registerSimpleBlockItem(BlockStateModelGenerator generator, Block block, String blockModel) {
+        generator.registerParentedItemModel(block, new Identifier(Apollo.MOD_ID, "block/" + blockModel));
     }
 
-    private static Model basicBlockItem(Item parent) {
-        Identifier parentIdentifier = parent.getRegistryEntry().registryKey().getValue();
-        return basicBlockItem(new Identifier(parentIdentifier.getNamespace(), "block/" + parentIdentifier.getPath()));
+    private static void registerCubeAll(BlockStateModelGenerator generator, Block block) {
+        generator.registerSimpleCubeAll(block);
     }
 
-    private static void registerBasicBlockItem(ItemModelGenerator itemModelGenerator, Item item) {
-        itemModelGenerator.register(item, basicBlockItem(item));
-    }
-
-    private static void registerBasicBlockItem(ItemModelGenerator itemModelGenerator, Item item, String parent) {
-        itemModelGenerator.register(item, basicBlockItem(parent));
-    }
-
-    private static void registerBasicBlockItem(ItemModelGenerator itemModelGenerator, Item item, Identifier parent) {
-        itemModelGenerator.register(item, basicBlockItem(parent));
+    private static void registerGeneratedItem(ItemModelGenerator generator, Item item) {
+        generator.register(item, Models.GENERATED);
     }
 }
