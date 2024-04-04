192a21dc52c2f1d1ae95e7527b4ad22fd8959ef4
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:52 -0400

    Clean up item group registry

--- src/main/java/com/thepinkhacker/apollo/item/ApolloItemGroups.java
+++ src/main/java/com/thepinkhacker/apollo/item/ApolloItemGroups.java
@@ -13,7 +13,7 @@ import net.minecraft.text.Text;
 public abstract class ApolloItemGroups {
     public static final RegistryKey<ItemGroup> APOLLO = of(
             "apollo",
-            createBuilder("apollo")
+            builder -> builder
                     .icon(() -> new ItemStack(ApolloItems.SPACE_SUIT_HELMET))
                     .entries(((context, entries) -> {
                         entries.add(ApolloItems.FUEL_BUCKET);
@@ -38,12 +38,11 @@ public abstract class ApolloItemGroups {
                         entries.add(ApolloItems.LAUNCHPAD);
                         entries.add(ApolloItems.SHUTTLE_WORKBENCH);
                     }))
-                    .build()
     );
 
     public static final RegistryKey<ItemGroup> MOON = of(
             "moon",
-            createBuilder("moon")
+            builder -> builder
                     .icon(() -> new ItemStack(ApolloItems.LUNAR_SOIL))
                     .entries(((context, entries) -> {
                         entries.add(ApolloItems.LUNAR_DUST);
@@ -52,18 +51,29 @@ public abstract class ApolloItemGroups {
                         entries.add(ApolloItems.LUNAR_COBBLESTONE);
                         entries.add(ApolloItems.LUNAR_IRON_ORE);
                     }))
-                    .build()
     );
 
     public static void register() {}
 
-    private static RegistryKey<ItemGroup> of(String id, ItemGroup itemGroup) {
+    private static RegistryKey<ItemGroup> of(String id, IdItemGroupBuilder builderCallback) {
         RegistryKey<ItemGroup> key = RegistryKey.of(RegistryKeys.ITEM_GROUP, Apollo.getIdentifier(id));
+
+        ItemGroup itemGroup = builderCallback
+                .getBuilder(createBuilder(id))
+                .displayName(Text.translatable("itemGroup." + Apollo.MOD_ID + "." + id))
+                .build();
+
         Registry.register(Registries.ITEM_GROUP, key, itemGroup);
+
         return key;
     }
 
     private static ItemGroup.Builder createBuilder(String id) {
         return FabricItemGroup.builder().displayName(Text.translatable("itemGroup." + Apollo.MOD_ID + "." + id));
+    }
+
+    @FunctionalInterface
+    private interface IdItemGroupBuilder {
+        ItemGroup.Builder getBuilder(ItemGroup.Builder builder);
     }
 }
