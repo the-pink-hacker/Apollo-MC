79da023f3ccd8934a3054ef958057d92cc24b316
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:49 -0400

    Improve moon sunset

--- src/main/java/com/ryangar46/apollo/mixin/client/render/WorldRendererMixin.java
+++ src/main/java/com/ryangar46/apollo/mixin/client/render/WorldRendererMixin.java
@@ -9,10 +9,13 @@ import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
+    private static final float NIGHT_ANGLE = 0.5f;
+
     @Shadow
     private ClientWorld world;
 
@@ -30,5 +33,17 @@ public abstract class WorldRendererMixin {
         } else {
             RenderSystem.setShaderTexture(i, identifier);
         }
+    }
+
+    @ModifyArg(
+            method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/render/DimensionEffects;getFogColorOverride(FF)[F"
+            ),
+            index = 0
+    )
+    private float getFogColor(float skyAngle) {
+        return world.method_40134().isIn(TagManager.ATMOSPHERE_NOT_VISIBLE_WORLDS) ? NIGHT_ANGLE : skyAngle;
     }
 }
