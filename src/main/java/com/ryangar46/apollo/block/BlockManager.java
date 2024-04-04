f423e6bd45f24cc860f2f3130da3f2cb149598f2
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:49 -0400

    Add lunar dust

--- src/main/java/com/ryangar46/apollo/block/BlockManager.java
+++ src/main/java/com/ryangar46/apollo/block/BlockManager.java
@@ -5,10 +5,7 @@ import com.ryangar46.apollo.fluid.FluidManager;
 import com.ryangar46.apollo.item.ItemGroupManager;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.FluidBlock;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -19,6 +16,7 @@ public class BlockManager {
     public static final Block FLUID_PIPE = new FluidPipeBlock(FabricBlockSettings.of(Material.METAL).strength(5.0f, 6.0f));
     public static final Block FUEL = new FluidBlock(FluidManager.STILL_FUEL, FabricBlockSettings.of(Material.WATER).noCollision().dropsNothing());
     public static final Block LAUNCH_PAD = new LaunchpadBlock(FabricBlockSettings.of(Material.METAL).strength(50.0f, 12000.0f).requiresTool());
+    public static final Block LUNAR_DUST = new FallingBlock(FabricBlockSettings.of(Material.SOIL).strength(1.0f));
     public static final Block LUNAR_COBBLESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.75f, 1.5f).requiresTool());
     public static final Block LUNAR_IRON_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3.5f, 2.5f).requiresTool());
     public static final Block LUNAR_SOIL = new Block(FabricBlockSettings.of(Material.SOIL).strength(1.0f));
@@ -35,6 +33,7 @@ public class BlockManager {
         registerBlock(FLUID_PIPE, "fluid_pipe", ItemGroupManager.APOLLO);
         registerBlock(FUEL, "fuel");
         registerBlock(LAUNCH_PAD, "launchpad", ItemGroupManager.APOLLO);
+        registerBlock(LUNAR_DUST, "lunar_dust", ItemGroupManager.MOON);
         registerBlock(LUNAR_COBBLESTONE, "lunar_cobblestone", ItemGroupManager.MOON);
         registerBlock(LUNAR_IRON_ORE, "lunar_iron_ore", ItemGroupManager.MOON);
         registerBlock(LUNAR_SOIL, "lunar_soil", ItemGroupManager.MOON);
