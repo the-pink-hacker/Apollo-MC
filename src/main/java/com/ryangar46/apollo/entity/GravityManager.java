2363b8afa67747d64fb3285647d4c3f433eaf487
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:49 -0400

    Make fall damage based off of gravity multiplier

--- src/main/java/com/ryangar46/apollo/entity/GravityManager.java
+++ src/main/java/com/ryangar46/apollo/entity/GravityManager.java
@@ -4,13 +4,11 @@ import com.ryangar46.apollo.world.dimension.DimensionManager;
 import net.minecraft.world.World;
 
 public class GravityManager {
-    public static final double DEFAULT_GRAVITY = 0.08d;
-
-    public static double getGravity(World world) {
+    public static double getGravityMutiplier(World world) {
         if (world.getRegistryKey() == DimensionManager.MOON) {
-            return DEFAULT_GRAVITY * 0.165d;
+            return 0.165d;
         } else {
-            return DEFAULT_GRAVITY;
+            return 1.0d;
         }
     }
 }
