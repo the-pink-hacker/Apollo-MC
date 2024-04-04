83ce7d22248c4494461ca97bdf718f72f6d55ed7
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:52 -0400

    Setup terrabelnder region

--- src/main/java/com/thepinkhacker/apollo/entry/ApolloTerraBlenderEntry.java
+++ src/main/java/com/thepinkhacker/apollo/entry/ApolloTerraBlenderEntry.java
@@ -1,8 +1,12 @@
 package com.thepinkhacker.apollo.entry;
 
+import com.thepinkhacker.apollo.world.region.OverworldRegion;
+import terrablender.api.Regions;
 import terrablender.api.TerraBlenderApi;
 
 public class ApolloTerraBlenderEntry implements TerraBlenderApi {
     @Override
-    public void onTerraBlenderInitialized() {}
+    public void onTerraBlenderInitialized() {
+        Regions.register(new OverworldRegion(10));
+    }
 }
---
+++ src/main/java/com/thepinkhacker/apollo/world/region/OverworldRegion.java
@@ -0,0 +1,28 @@
+package com.thepinkhacker.apollo.world.region;
+
+import com.mojang.datafixers.util.Pair;
+import com.thepinkhacker.apollo.Apollo;
+import com.thepinkhacker.apollo.world.biome.ApolloBiomeKeys;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.biome.source.util.MultiNoiseUtil;
+import terrablender.api.Region;
+import terrablender.api.RegionType;
+
+import java.util.function.Consumer;
+
+public class OverworldRegion extends Region {
+    private static final Identifier NAME = Apollo.getIdentifier("overworld");
+
+    public OverworldRegion(int weight) {
+        super(NAME, RegionType.OVERWORLD, weight);
+    }
+
+    @Override
+    public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> mapper) {
+        addBiomeSimilar(mapper, BiomeKeys.DESERT, ApolloBiomeKeys.OIL_DESERT);
+    }
+}
