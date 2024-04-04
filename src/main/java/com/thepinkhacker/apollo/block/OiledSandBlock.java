9add3fddddfcefb7074ee91538d519d3b8195e93
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:52 -0400

    Add dust particle effect to oiled sand

--- src/main/java/com/thepinkhacker/apollo/block/OiledSandBlock.java
+++ src/main/java/com/thepinkhacker/apollo/block/OiledSandBlock.java
@@ -2,12 +2,19 @@ package com.thepinkhacker.apollo.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FallingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.client.util.ParticleUtil;
 import net.minecraft.entity.ai.pathing.NavigationType;
+import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class OiledSandBlock extends Block {
     private static final VoxelShape COLLISION_SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0);
@@ -39,5 +46,16 @@ public class OiledSandBlock extends Block {
     @Override
     public float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
         return 0.2f;
+    }
+
+    public static boolean canFallThrough(BlockState state) {
+        return state.isAir() || state.isIn(BlockTags.FIRE) || state.isLiquid() || state.isReplaceable();
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (random.nextInt(16) == 0 && canFallThrough(world.getBlockState(pos.down()))) {
+            ParticleUtil.spawnParticle(world, pos, random, new BlockStateParticleEffect(ParticleTypes.FALLING_DUST, state));
+        }
     }
 }
