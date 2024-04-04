074d6f0d662e95b97f8fa1fde7af008cdacfce65
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:48 -0400

    Add shuttle

--- src/main/java/com/ryangar46/apollo/block/BlockManager.java
+++ src/main/java/com/ryangar46/apollo/block/BlockManager.java
@@ -12,11 +12,15 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockManager {
-    public static final LaunchpadBlock LAUNCH_PAD = new LaunchpadBlock(FabricBlockSettings.of(Material.METAL).strength(50.0f, 12000.0f).requiresTool());
+    public static final Block LAUNCH_PAD = new LaunchpadBlock(FabricBlockSettings.of(Material.METAL).strength(50.0f, 12000.0f).requiresTool());
     public static final Block LUNAR_COBBLESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.75f, 1.5f).requiresTool());
     public static final Block LUNAR_IRON_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3.5f, 2.5f).requiresTool());
     public static final Block LUNAR_SOIL = new Block(FabricBlockSettings.of(Material.SOIL).strength(1.0f));
     public static final Block LUNAR_STONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f).requiresTool());
+    public static final Block SHUTTLE_COCKPIT = new ShuttleCockpit(FabricBlockSettings.of(Material.METAL).strength(5, 6).requiresTool());
+    public static final Block SHUTTLE_ENGINE = new ShuttleEngineBlock(FabricBlockSettings.of(Material.METAL).strength(5, 6).requiresTool());
+    public static final Block SHUTTLE_FUEL_STORAGE = new Block(FabricBlockSettings.of(Material.METAL).strength(5, 6).requiresTool());
+    public static final Block SHUTTLE_NOSE = new ShuttleNoseBlock(FabricBlockSettings.of(Material.METAL).strength(5, 6).requiresTool());
 
     public static void register() {
         Apollo.LOGGER.info("Registering blocks");
@@ -25,6 +29,10 @@ public class BlockManager {
         registerBlock(LUNAR_IRON_ORE, "lunar_iron_ore", ItemGroupManager.MOON);
         registerBlock(LUNAR_SOIL, "lunar_soil", ItemGroupManager.MOON);
         registerBlock(LUNAR_STONE, "lunar_stone", ItemGroupManager.MOON);
+        registerBlock(SHUTTLE_COCKPIT, "shuttle_cockpit", ItemGroupManager.APOLLO);
+        registerBlock(SHUTTLE_ENGINE, "shuttle_engine", ItemGroupManager.APOLLO);
+        registerBlock(SHUTTLE_FUEL_STORAGE, "shuttle_fuel_storage", ItemGroupManager.APOLLO);
+        registerBlock(SHUTTLE_NOSE, "shuttle_nose", ItemGroupManager.APOLLO);
     }
 
     private static void registerBlock(Block block, String id, ItemGroup itemGroup) {
--- src/main/java/com/ryangar46/apollo/world/dimension/DimensionManager.java
+++ src/main/java/com/ryangar46/apollo/world/dimension/DimensionManager.java
@@ -4,16 +4,16 @@ import com.ryangar46.apollo.Apollo;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.dimension.DimensionType;
 
 public class DimensionManager {
+    private static final RegistryKey<DimensionOptions> MOON_OPTIONS = RegistryKey.of(Registry.DIMENSION_KEY, new Identifier(Apollo.MOD_ID, "moon"));
+    public static RegistryKey<World> MOON = RegistryKey.of(Registry.WORLD_KEY, MOON_OPTIONS.getValue());
+    private static final RegistryKey<DimensionType> MOON_TYPE = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier(Apollo.MOD_ID, "moon"));
+
     public static void register() {
         Apollo.LOGGER.info("Registering dimensions");
-        registerDimension("moon");
-    }
-
-    private static void registerDimension(String id) {
-        RegistryKey.of(Registry.WORLD_KEY, RegistryKey.of(Registry.DIMENSION_KEY,
-                new Identifier(Apollo.MOD_ID, id)).getValue());
-        RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier(Apollo.MOD_ID, id));
     }
 }
