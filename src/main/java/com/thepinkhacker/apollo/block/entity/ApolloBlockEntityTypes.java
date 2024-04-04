660e53ba2906e5b63e8e5e53421742b77262fbdc
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:50 -0400

    Add method for registering entities

--- src/main/java/com/thepinkhacker/apollo/block/entity/ApolloBlockEntityTypes.java
+++ src/main/java/com/thepinkhacker/apollo/block/entity/ApolloBlockEntityTypes.java
@@ -3,16 +3,23 @@ package com.thepinkhacker.apollo.block.entity;
 import com.thepinkhacker.apollo.Apollo;
 import com.thepinkhacker.apollo.block.ApolloBlocks;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ApolloBlockEntityTypes {
-    public static final BlockEntityType<FluidPipeBlockEntity> FLUID_PIPE_BLOCK_ENTITY = Registry.register(
-            Registries.BLOCK_ENTITY_TYPE,
-            new Identifier(Apollo.MOD_ID, "fluid_pipe"),
+    public static final BlockEntityType<FluidPipeBlockEntity> FLUID_PIPE_BLOCK_ENTITY = of(
+            "fluid_pipe",
             FabricBlockEntityTypeBuilder.create(FluidPipeBlockEntity::new, ApolloBlocks.FLUID_PIPE)
-                    .build()
     );
+
+    private static <T extends BlockEntity> BlockEntityType<T> of(String id, FabricBlockEntityTypeBuilder<T> builder) {
+        return Registry.register(
+                Registries.BLOCK_ENTITY_TYPE,
+                new Identifier(Apollo.MOD_ID, id),
+                builder.build()
+        );
+    }
 }
