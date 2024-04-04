40a3a83cb508753bfd18b3bbd700272881ebb0cd
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:52 -0400

    Add join event for syncing space bodies

--- src/main/java/com/thepinkhacker/apollo/server/ApolloServerEvents.java
+++ src/main/java/com/thepinkhacker/apollo/server/ApolloServerEvents.java
@@ -5,6 +5,7 @@ import com.thepinkhacker.apollo.resource.SpaceBodyManager;
 import com.thepinkhacker.apollo.world.dimension.SpaceBody;
 import com.thepinkhacker.apollo.world.spawner.GenericSpawnerManager;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -17,7 +18,6 @@ import java.util.List;
 public abstract class ApolloServerEvents {
     public static void register() {
         ServerLifecycleEvents.SERVER_STARTED.register(server -> GenericSpawnerManager.register());
-        // TODO: Sync space bodies on player join
         ServerLifecycleEvents.END_DATA_PACK_RELOAD.register((server, resourceManager, success) -> {
             if (!success) return;
 
@@ -26,6 +26,9 @@ public abstract class ApolloServerEvents {
             for (ServerPlayerEntity player : getNonHostPlayers(server)) {
                 ServerPlayNetworking.send(player, new SyncSpaceBodiesPacket(spaceBodies));
             }
+        });
+        ServerPlayConnectionEvents.JOIN.register((networkHandler, packetSender, server) -> {
+            packetSender.sendPacket(new SyncSpaceBodiesPacket(SpaceBodyManager.getInstance().getSpaceBodies()));
         });
     }
 
