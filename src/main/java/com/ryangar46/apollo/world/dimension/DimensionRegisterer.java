d978853bda7c02914971a809cd75532fed609c32
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:48 -0400

    Add lunar highlands

--- src/main/java/com/ryangar46/apollo/world/dimension/DimensionRegisterer.java
+++ src/main/java/com/ryangar46/apollo/world/dimension/DimensionRegisterer.java
@@ -7,13 +7,14 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
 
-public class ModDimensions {
+public class DimensionRegisterer {
     private static final RegistryKey<DimensionOptions> DIMENSION_KEY = RegistryKey.of(Registry.DIMENSION_KEY,
             new Identifier(Apollo.MOD_ID, "moon"));
     public static RegistryKey<World> APOLLO_DIMENSION_KEY = RegistryKey.of(Registry.WORLD_KEY, DIMENSION_KEY.getValue());
     private static final RegistryKey<DimensionType> APOLLO_DIMENSION_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY,
-            new Identifier(Apollo.MOD_ID, "moon_type"));
+            new Identifier(Apollo.MOD_ID, "moon"));
 
     public static void register() {
         Apollo.LOGGER.info("Registering dimensions");
