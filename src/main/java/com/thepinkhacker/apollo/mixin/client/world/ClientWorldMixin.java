40e136492ff2a472f3335612806b7b60fee2bff2
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:51 -0400

    Add inject for skybox

--- src/main/java/com/thepinkhacker/apollo/mixin/client/world/ClientWorldMixin.java
+++ src/main/java/com/thepinkhacker/apollo/mixin/client/world/ClientWorldMixin.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-// TODO: Separate Apollo skyboxes from overworld
 @Mixin(ClientWorld.class)
 public abstract class ClientWorldMixin {
     @ModifyVariable(
