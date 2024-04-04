99419c90e29abbc8fe262dcc9500fbfab9bce1c4
Pink Garrett <pink@thepinkhacker.com>
2024-04-04 04:10:48 -0400

    Add lunar iron ore

--- src/main/java/com/ryangar46/apollo/block/BlockManager.java
+++ src/main/java/com/ryangar46/apollo/block/BlockManager.java
@@ -3,7 +3,6 @@ package com.ryangar46.apollo.block;
 import com.ryangar46.apollo.Apollo;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
@@ -12,13 +11,15 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BlockManager {
-    public static final Block LUNAR_COBBLESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.75f, 2.0f).requiresTool());
+    public static final Block LUNAR_COBBLESTONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.75f, 1.5f).requiresTool());
+    public static final Block LUNAR_IRON_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3.5f, 2.5f).requiresTool());
     public static final Block LUNAR_SOIL = new Block(FabricBlockSettings.of(Material.SOIL).strength(1.0f));
-    public static final Block LUNAR_STONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f, 2.0f).requiresTool());
+    public static final Block LUNAR_STONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5f).requiresTool());
 
     public static void register() {
         Apollo.LOGGER.info("Registering blocks");
         registerBlock(LUNAR_COBBLESTONE, "lunar_cobblestone", ItemGroup.BUILDING_BLOCKS);
+        registerBlock(LUNAR_IRON_ORE, "lunar_iron_ore", ItemGroup.BUILDING_BLOCKS);
         registerBlock(LUNAR_SOIL, "lunar_soil", ItemGroup.BUILDING_BLOCKS);
         registerBlock(LUNAR_STONE, "lunar_stone", ItemGroup.BUILDING_BLOCKS);
     }
